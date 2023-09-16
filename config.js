c18c8e21d9aa423094d72a446c1743f48501a34b
BotDevilLozan <118263959+BotDevilLozan@users.noreply.github.com>
2023-09-16 20:33:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -11,21 +11,21 @@ global.antitags = true;
 
 //auto functioner
 global.autoTyping = false;                //auto tying by default off.
-global.autoRecord = false;                //auto recording by default off.
+global.autoRecord = true;                //auto recording by default off.
 global.groupevent = false;                //This is the new variable for controlling group event handling.
 global.statusseen = true;                 //This is the new variable for controlling status seen.
 
 
 //
-global.Owner = ["8801853262586","8801928502256"];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.OwnerNumber = ["8801853262586","8801928502256"];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.ownertag = ["8801928502256"];
-global.OwnerName = "Slasher";
-global.BotName = "XLICON-MD";
+global.Owner = ["94773010580","94773010580"];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.OwnerNumber = ["94773010580","94773010580"];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.ownertag = ["94773010580"];
+global.OwnerName = "FENIXXX";
+global.BotName = "FENIX-MOTION'Z";
 global.packname = "X-2.0 Bot";                             //Do not change.
-global.author = "By: Slasher";                               //Do not change.
-global.BotSourceCode = "https://github.com/ahil15/Xlicon-v2"; //Do not change.
-global.SupportGroupLink = "https://chat.whatsapp.com/EjsQvJNcFGVCSfaBEIxZm2"; 
+global.author = "By: 𝙁𝙀𝙉𝙄𝙓𝙓𝙓";                               //Do not change.
+global.BotSourceCode = "fenixprogrammerz@gmail.com"; //Do not change.
+global.SupportGroupLink = ""; 
 
 
 //
@@ -33,7 +33,7 @@ global.sessionName = "session";                          //Do not change.
 
 
 //
-global.prefa = ["."];                                    //Default prefix here.
+global.prefa = [":"];                                    //Default prefix here.
 global.openAiAPI = "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";
 
 
